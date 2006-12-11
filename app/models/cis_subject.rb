638df2892614e94d45ad869c1063576d623796b1
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2006-12-11 02:06:41 +0000

    modified autocomplete

--- app/models/cis_subject.rb
+++ app/models/cis_subject.rb
@@ -1,3 +1,24 @@
 class CisSubject < ActiveRecord::Base
   has_many :cis_courses
+  
+  def self.search_for_course(name)
+    name.upcase!
+    
+    (subject, number) = name.scan(/^([A-Z]*)\s*(\d*)$/)[0]
+
+    if number.blank?
+      subjects = find :all,
+        :conditions => [ 'cis_subjects.code LIKE ?', subject + '%' ],
+        :limit => 10
+    else
+      subjects = find :all,
+        :conditions => [ 'cis_subjects.code = ? AND cis_courses.number LIKE ?',
+                          subject,
+                          number + '%' ],
+        :joins => "LEFT JOIN cis_courses ON cis_courses.cis_subject_id = cis_subjects.id",
+        :limit => 10
+    end
+    
+    return subjects    
+  end
 end
