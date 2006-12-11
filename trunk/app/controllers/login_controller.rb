2212a83c25aea90cb7906314228b124ef3f63c5a
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2006-12-11 05:51:32 +0000

    added account creation/editing added dependencies and dependency creation in models

--- trunk/app/controllers/login_controller.rb
+++ trunk/app/controllers/login_controller.rb
@@ -5,7 +5,7 @@ class LoginController < ApplicationController
     @user = User.authenticate(params[:user], params[:pass])
     if @user != nil
       @session[:user] = @user.id
-      redirect_to(:controller => 'long_term')
+      redirect_to :controller => 'profile'
     else
       render :action => 'index'
       flash[:error] = "Invalid user name or password."
--- trunk/app/models/semester.rb
+++ trunk/app/models/semester.rb
@@ -1,6 +1,14 @@
 class Semester < ActiveRecord::Base
   has_and_belongs_to_many :cis_courses
-  has_one :course_plan
+  has_one :course_plan, :dependent => :destroy
   belongs_to :user
 
+  before_create :create_dependancies
+  
+  
+  private
+  def create_dependancies
+    create_course_plan()
+  end
+  
 end
