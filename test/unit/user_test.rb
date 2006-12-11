a7860816f758d6ad6cce6285a56571688a332d6f
nsonie2 <nsonie2@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2006-12-11 03:55:58 +0000

    unit testing for user object

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,10 +1,27 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < Test::Unit::TestCase
-  fixtures :users
 
+  fixtures :users
+#  fixtures :username_password_fail_cases, :username_password_pass_cases
   # Replace this with your real tests.
   def test_truth
     assert true
   end
+ 
+  def test_username_passaword_fail_cases
+   
+    assert_equal("james", User.authenticate("james","bond").username)
+    
+    #assert_nil(user.find_by_username(nikhil.username))
+  end
+  
+#  def test_username_password_pass_case
+#  
+#   #create a user object
+#    assert_equal("james", User.authenticate("james","bond").username)
+#    assert_equal("bond", User.authenticate("james","bond").password_hash)
+# end
+  
+  
 end
