f83867e7545e75411351a57d6520716ce8a4d604
nsonie2 <nsonie2@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2006-12-12 01:14:39 +0000

    unit tested user_test.rb, cis_subject_test.rb
    Remark: one of the test in cis_subject_test.rb fails

--- trunk/test/unit/user_test.rb
+++ trunk/test/unit/user_test.rb
@@ -2,7 +2,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < Test::Unit::TestCase
 
-  fixtures :users
+ fixtures :users
+
 #  fixtures :username_password_fail_cases, :username_password_pass_cases
   # Replace this with your real tests.
   def test_truth
@@ -10,18 +11,20 @@ class UserTest < Test::Unit::TestCase
   end
  
   def test_username_passaword_fail_cases
-   
-    assert_equal("james", User.authenticate("james","bond").username)
-    
-    #assert_nil(user.find_by_username(nikhil.username))
+    #finding something that is not in database
+    #to check if it returning nil 
+    assert_nil User.authenticate("nikhil","sonie")
   end
   
-#  def test_username_password_pass_case
-#  
-#   #create a user object
-#    assert_equal("james", User.authenticate("james","bond").username)
-#    assert_equal("bond", User.authenticate("james","bond").password_hash)
-# end
-  
-  
+ def test_username_password_pass_case
+  #find something that in the database
+  #check if the username and password are stored correctly 
+    assert_equal("james", User.authenticate("james","bond").username)
+    assert_equal(Digest::SHA1.hexdigest("bond"), User.authenticate("james","bond").password_hash)
+ end
+ 
+ def test_validate_uniqueness
+
+ end
+ 
 end
