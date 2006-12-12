b38e05113f1090ea534451434614dae54999eed3
kizior <kizior@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2006-12-12 21:42:01 +0000

    maybe i'm committing too much?

--- trunk/app/controllers/friends.rb
+++ trunk/app/controllers/friends.rb
@@ -4,7 +4,7 @@ class LoginController < ApplicationController
     redirect_to :action => 'friends_list'
   end
 
-  def freinds_list
+  def friends_list
     if params[:id] != nil
       @user = User.find(params[:id])
     else
@@ -12,3 +12,4 @@ class LoginController < ApplicationController
     end
     @title = 'Friends Page of ' + @user.first_name + ' ' + @user.last_name
   end
+end
