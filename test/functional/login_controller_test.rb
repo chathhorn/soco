603369fe7544ff84eda12c54e05b3b8064795268
nsonie2 <nsonie2@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2006-12-12 01:16:21 +0000

    started doing functional testing on login_controller_test.rb . doesnt work correctly for now

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -11,8 +11,28 @@ class LoginControllerTest < Test::Unit::TestCase
     @response   = ActionController::TestResponse.new
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  #gets the index page successfully?
+  def test_index
+    get :index
+    assert_response :success
   end
+  
+  #check for login failure
+  def test_login_failure
+    get :validate, "username"=>"nikhil", "password_hash"=>"sonie"
+    assert_not_nil assigns["user"]
+    assert_nil assigns["user"]
+    assert assign.empty?
+    assert flash.has_error?
+  end
+  
+  #check for login success
+  def test_login_success
+    get :validate, "username"=>"james", "password_hash"=>"bond"
+    assert_not_nit assigns["user"]
+    assert_equal 1, assigns["user"].id
+    assert flash.empty?   
+  end    
+  
+  
 end
