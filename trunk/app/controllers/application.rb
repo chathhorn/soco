42f04a9b197b5e4bb03e5c91b012a87b85dbaf99
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-02-13 09:13:04 +0000

    upgraded to Rails 1.2 issue 0000019

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -1,6 +1,10 @@
-# Filters added to this controller will be run for all controllers in the application.
+# Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
+
 class ApplicationController < ActionController::Base
+  # Pick a unique cookie name to distinguish our session data from others'
+  session :session_key => '_soco_session_id'
+
   before_filter :authenticate
   
   
