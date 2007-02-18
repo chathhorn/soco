c4b92f5513066c57e9e1063faf850e77414cec3f
nsonie2 <nsonie2@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-02-18 21:56:23 +0000

    Modified controllers/profile/friends_controller.rb issue 5

--- branches/friends/app/controllers/profile/friends_controller.rb
+++ branches/friends/app/controllers/profile/friends_controller.rb
@@ -1,2 +1,35 @@
 class Profile::FriendsController < ApplicationController
+
+  def index
+    @title = ''
+    @user = User.find(session[:user])
+    @course_bin_courses = @user.course_bin.cis_courses
+    @semesters = @user.semesters.find(:all, :order => 'year ASC, semester ASC')
+  end
+
+
+  def search
+  
+  end
+  
+  def list
+  
+  end
+  
+  def browse
+  
+  end
+
+  def add
+  
+ 
+  end
+ 
+  def remove
+    user = User.find_by_id session[:user]
+    friend = User.find_by_id params[:id]
+    user.friends.delete friend
+    redirect_to :action => 'show'
+  end
+   
 end
