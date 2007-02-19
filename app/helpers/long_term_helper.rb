f5ed14588900ecbd399e688553c89783dbbb4491
mhines3 <mhines3@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-02-19 09:31:15 +0000

    Happy now, Matt? issue 24

--- app/helpers/long_term_helper.rb
+++ app/helpers/long_term_helper.rb
@@ -9,9 +9,20 @@ module LongTermHelper
       return 'FA'
     end
   end
+
+  def lookup_semester_word time
+    code = lookup_semester time
+    if (code == 'SP')
+      return 'Spring'
+    elsif (code == 'FA')
+      return 'Fall'
+    else (code == 'SU')
+      return 'Summer'
+    end
+  end
   
   def lookup_year time
-      if (time.month == 12) 
+    if (time.month == 12) 
       return (time.year + 1).to_s;
     else 
       return time.year.to_s
