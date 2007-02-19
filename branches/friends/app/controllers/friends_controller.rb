69ef618e21e0b1f566a2ab19b887f5eb2396a1d2
mirani <mirani@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-02-19 02:56:54 +0000

    Implemented search, list, browse, add and remove in friends_controller.rb Added the search function in user.rb Added link for the new functionalities in show.rhtml issue 5

--- branches/friends/app/controllers/friends_controller.rb
+++ branches/friends/app/controllers/friends_controller.rb
@@ -8,28 +8,43 @@ class FriendsController < ApplicationController
 
 
   def search
-  
+    @user = User.find(session[:user])
+    if (params[:q])
+      @friends = User.search(params[:q])
+    else
+      @friends = []
+    end
+    render :action => 'list'
   end
   
   def list
     @user = User.find(session[:user])
-    @friends = @user.friends.slice(1,5)
+    @friends = @user.friends.slice(0,10)
   end
   
   def browse
-  
+    @user = User.find(session[:user])
+    @friends = User.find :all, :order => 'last_name ASC, first_name ASC'
+    render :action => 'list'
   end
 
   def add
     user = User.find session[:user]
- 
+    friend = User.find params[:id]
+    
+    user.friends.concat friend
+    friend.friends.concat user
+    
+    redirect_to :controller => 'profile', :action => 'show'
   end
  
   def remove
     user = User.find session[:user]
     friend = User.find params[:id]
-    user.friends.delete friend.id
-    friend.friends.delete user.id
-    redirect_to :controller => '/profile', :action => 'show'
+    
+    user.friends.delete friend
+    friend.friends.delete user
+    
+    redirect_to :controller => 'profile', :action => 'show'
   end
 end
