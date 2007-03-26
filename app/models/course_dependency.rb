a4b1228601db9ab7cb0dfb574a4233925b6bda0f
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-03-26 09:20:32 +0000

    Changed type column to node_type due to compatibility issues Parser is more compatible now issue 50

--- app/models/course_dependency.rb
+++ app/models/course_dependency.rb
@@ -1,19 +1,23 @@
 class CourseDependency < ActiveRecord::Base
-  acts_as_graph
+  acts_as_graph :edge_table => 'course_dependency_edges'
   has_many :cis_courses
   
   def to_s
-    if type == :COURSE
-      return cis_courses[0].to_s
-    end
-  
     courses = []
-    children.each { |child| courses.push child.to_s }
+    children.each { |child| courses.push child.to_s_helper }
     
-    if type == :OR
+    if node_type == :OR
       return courses.join(" or ")
     end
     
     return courses.join(" and ")
+  end
+  
+  def to_s_helper
+    if node_type == :COURSE
+      return cis_courses[0].to_s
+    end
+    
+    return to_s
   end
 end
