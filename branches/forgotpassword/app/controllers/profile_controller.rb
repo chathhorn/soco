ab63e8ae33bdb8449bd3df93321bfa25ef8a5c6c
mhines3 <mhines3@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-03-26 16:02:25 +0000

    issue 77

---
+++ branches/forgotpassword/app/controllers/profile_controller.rb
@@ -0,0 +1,51 @@
+class ProfileController < ApplicationController
+  def index
+    redirect_to :action => 'show'
+  end
+
+  def show
+    if params[:id] != nil
+      @user = User.find(params[:id])
+    else
+      @user = User.find(session[:user])
+    end
+    @friends = @user.friends
+    @title = 'Profile for ' + @user.first_name + ' ' + @user.last_name
+  end
+
+  def register
+    @title = 'Register'
+    if (params[:user] != nil)
+      @user = User.new(params[:user])
+      if @user.save
+        session[:user] = @user.id
+        flash[:notice] = 'Your account is now created!'
+        redirect_to :action => 'show'
+      end
+    else
+      @user = User.new()
+    end
+    @colleges = College.find(:all, :order => 'name ASC')
+    @majors = Major.find(:all, :order => 'name ASC')
+  end
+
+  def edit
+    @user = User.find(session[:user])
+    if params[:user]
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'Your changes have been successfully saved.'
+        redirect_to :action => 'show', :id => @user
+        return
+      end
+    end
+
+    @title = 'Change Profile'
+    @colleges = College.find(:all, :order => 'name ASC')
+    @majors = Major.find(:all, :order => 'name ASC')
+  end
+
+  def destroy
+    User.find(session[:user]).destroy
+    redirect_to :controller => 'login'
+  end
+end
