d42560786b969f05dface1e9747c7533465c1ece
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-03-27 08:03:13 +0000

    Removed unnecessary merges from facebook branch removed public/index.html which prevented proper application usage issue 69

--- trunk/test/unit/course_dependency_test.rb
+++ trunk/test/unit/course_dependency_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CourseDependencyTest < Test::Unit::TestCase
-  fixtures :course_dependencies
+  fixtures :course_dependencies, :course_dependency_edges
 
   # Replace this with your real tests.
   def test_truth
