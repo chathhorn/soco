42f50f93e6f9539f2e7123ad86d748a79608c887
pgits2 <pgits2@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-03-27 03:19:40 +0000

    issue 0000069

--- trunk/test/unit/cis_section_test.rb
+++ trunk/test/unit/cis_section_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CisSectionTest < Test::Unit::TestCase
-  fixtures :cis_sections
+#  fixtures :cis_sections
 
   # Replace this with your real tests.
   def test_truth
