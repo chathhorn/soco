66039127ff4ce56160bc4ea551818b2366fdbb4b
apradom2 <apradom2@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-03-30 21:51:36 +0000

    issue 0000018

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,11 @@
 class LoginController < ApplicationController
+layout "blank"
+
+  
   def index
     if session[:user]
-      redirect_to :controller => 'profile', :action => 'show'
+     redirect_to :controller => 'profile', :action => 'show'
+
     end
 
     @title = "Login"
@@ -14,13 +18,39 @@ class LoginController < ApplicationController
       redirect_to :controller => 'profile'
     else
       flash[:error] = "Invalid user name or password."
-      redirect_to :action => 'index'
+      redirect_to :action => 'welcome'
     end
   end
   
   def logout
     reset_session
     flash[:alert] = "Logged out" 
-    redirect_to :action => "index" 
+    redirect_to :action => "welcome" 
+#    redirect_to "login.html"
   end
+  
+
+  
+  
+  def register
+    @title = 'Register'
+    if (params[:user] != nil)
+      @user = User.new(params[:user])
+      if @user.save
+        session[:user] = @user.id
+        flash[:notice] = 'Your account is now created!'
+        redirect_to :action => 'welcome'
+#        @@duh = "<script>parent.location='/profile/show';</script>"
+#@duh
+      end
+    else
+      @user = User.new()
+    end
+    @colleges = College.find(:all, :order => 'name ASC')
+    @majors = Major.find(:all, :order => 'name ASC')
+  end  
+  
+  
+  
+  
 end
