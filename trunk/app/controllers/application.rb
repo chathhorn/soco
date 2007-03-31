f5a3240e19f2b16b5ec448548efb1affa13483be
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-03-31 04:52:05 +0000

    Made html standards compliant cleaned up html fixed some performance issues fixed some display issues fixed some regressions moved authentication removal to login and profile/register issue 18

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -2,18 +2,16 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_soco_session_id'
 
   before_filter :authenticate
-  
-  
+
   #everything after is private
   private
   
   def authenticate
-    if session[:user] == nil && (params[:controller] != 'login' && !(params[:controller] == 'profile' && params[:action] == 'register'))
+    if session[:user] == nil
       redirect_to :controller => "login"
     end
   end
