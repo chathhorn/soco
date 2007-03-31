0886c8cd6ac7bb0b6ecefdecd9b7cce6dbc7e4d5
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-03-31 00:40:15 +0000

    reverted database.yml removed application controller code from blank controller issue 18

--- app/controllers/blank.rb
+++ app/controllers/blank.rb
@@ -2,18 +2,5 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class BlankController < ActionController::Base
-  # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_soco_session_id'
 
-  before_filter :authenticate
-  
-  
-  #everything after is private
-  private
-  
-  def authenticate
-    if session[:user] == nil && (params[:controller] != 'login' && !(params[:controller] == 'profile' && params[:action] == 'register'))
-      redirect_to :controller => "login"
-    end
-  end
 end
