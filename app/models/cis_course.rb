bd4de1cbaa73ebec3f50ff3d2ddb4f80cc1e7e24
chorneck <chorneck@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-01 04:21:46 +0000

    This feature is in a semi-working state.  Right now, when update_semester is called in the long_term_controller, the dependencies are checked for the course that was moved.  If !dependencies_satisfied then the course header turns red (hopefully).  Would like to check the dependencies on all courses when you move one course but I havent figure out how we can do that yet. issue 12

--- app/models/cis_course.rb
+++ app/models/cis_course.rb
@@ -1,11 +1,22 @@
 class CisCourse < ActiveRecord::Base
+
   has_many :cis_semesters
   belongs_to :cis_subject
   belongs_to :course_dependency
   has_and_belongs_to_many :course_bins
   has_and_belongs_to_many :semesters
   
+  def dependencies_satisfied
+    @dependencies_satisfied
+  end
+  
+  def dependencies_satisfied=(ds)
+    @dependencies_satisfied = ds
+  end
+   
   def to_s
     return cis_subject.to_s + ' ' + number.to_s
   end
+  
+
 end
