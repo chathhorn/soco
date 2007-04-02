23730e7bd0eaa194f8a0b3933fad3ec880bf76d9
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-02 03:41:05 +0000

    updated database creation script to upgrade only by default issue 116

--- trunk/components/local_database_creation/create_database.pl
+++ trunk/components/local_database_creation/create_database.pl
@@ -1,9 +1,14 @@
-#!c:/perl/bin/perl.exe -w
+#!env perl -w
 
 $ENV{PATH} = $ENV{PATH} . ';c:\Program Files\mySQL\MySQL Server 5.0\bin';
 
-print "[*    ] Creating Database and User\n";
-system("mysql -f -u root < CREATE.SQL") and die "Can't create database and user\n";
+if ($#ARGV >= 0 && $ARGV[0] eq '--create') {
+	print "Enter MySQL password for user 'root': ";
+	$password = <STDIN>;
+
+	print "[*    ] Creating Database and User\n";
+	system("mysql -f -u root -p$password < CREATE.SQL") and die "Can't create database and user\n";
+}
 
 chdir("../..");
 
@@ -11,12 +16,12 @@ print "[**   ] Running migrations to create tables\n";
 system("rake db:migrate") and die "Migration failed\n";
 
 print "[***  ] Clearing existing CIS course data\n";
-system("mysql -u root < \"db/data/development/TRUNCATE CIS Data.sql\"") and die "Truncate failed\n";
+system("mysql -u soco -pmonkey < \"db/data/development/TRUNCATE CIS Data.sql\"") and die "Truncate failed\n";
 
-print "[*** ] Importing CIS course data\n";
-system("mysql -u root < \"db/data/development/CIS Data.sql\"") and die "Data import failed\n";
+print "[**** ] Importing CIS course data\n";
+system("mysql -u root -pmonkey < \"db/data/development/CIS Data.sql\"") and die "Data import failed\n";
 
-print "[****] Creating testing tables\n";
+print "[*****] Creating testing tables\n";
 system("rake db:test:clone_structure") and die "Testing table creation failed\n";
 
 print "Finished!\n";
