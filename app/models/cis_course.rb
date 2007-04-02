e710bc7e6f99809271520f4b19b7307112771343
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-02 06:04:59 +0000

    Merged into trunk issue 12

--- app/models/cis_course.rb
+++ app/models/cis_course.rb
@@ -8,4 +8,14 @@ class CisCourse < ActiveRecord::Base
   def to_s
     return cis_subject.to_s + ' ' + number.to_s
   end
+  
+  def dependencies_satisfied(semester_id, user_id)
+    if semester_id == -1
+      return true
+    end
+    
+    user = User.find(user_id)
+   
+    return course_dependency.is_satisfied?(id, semester_id, user)
+  end
 end
--- app/models/course_dependency.rb
+++ app/models/course_dependency.rb
@@ -24,4 +24,57 @@ class CourseDependency < ActiveRecord::Base
     
     return to_s
   end
+  
+  def is_satisfied?(course_id, semester_id, user)
+    children.each do |child|
+      if not child.is_satisfied_helper?(course_id, semester_id, user)
+        return false
+      end
+    end
+
+    return true
+  end
+  
+  protected
+  def is_satisfied_helper?(course_id, semester_id, user)
+    case node_type
+      when :COURSE
+        return look_for_course_in_earlier_semester(course_id, semester_id, user) 
+      when :OR
+        children.each do |child|
+          if child.is_satisfied_helper?(course_id, semester_id, user)
+            return true
+          end
+        end
+        return false
+      when :AND
+        result = true
+        children.each do |child|
+          result &= child.is_satisfied_helper?(course_id, semester_id, user)
+        end
+        return result
+      else #:CONCURRENT
+        result = true
+        children.each do |child|
+          result &= child.is_satisfied_helper?(course_id, semester_id + 1, user)
+        end
+        return result
+    end
+  end
+  
+  private
+  def look_for_course_in_earlier_semester(course_id, max_semester_id, user)
+    user.semesters.each do |semester|
+      if semester.id == max_semester_id
+        break
+      end
+      semester.cis_courses.each do |course|
+        if course.course_dependency.id == id
+          return true
+        end
+      end
+    end
+
+    return false
+  end
 end
