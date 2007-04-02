a2142ceac4ed1f8eeae9c75077ee5dbef3ff8905
chorneck <chorneck@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-02 02:32:00 +0000

    Changed :password_hash to :password in all User.new's to fix errors issue 119

--- test/unit/user_friends_test.rb
+++ test/unit/user_friends_test.rb
@@ -11,7 +11,7 @@ class UserFriendsTest < Test::Unit::TestCase
 
 #this test shows that you can add a new user
   def test_add_new_user
-      user = User.new(:username=>'forest', :password_hash=>Digest::SHA1.hexdigest("gump"), :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
+      user = User.new(:username=>'forest', :password=>"gump", :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
       user.save
       puts user.id
       assert_equal('forest', user.username)
@@ -21,14 +21,14 @@ class UserFriendsTest < Test::Unit::TestCase
   
 # this test shows that when you initialize a new user, he has no friends by default   
   def test_zerofriends
-    user = User.new(:username=>'forest', :password_hash=>Digest::SHA1.hexdigest("gump"), :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
+    user = User.new(:username=>'forest', :password=>"gump", :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
     assert_equal(user.friends, [])
   end
 
 #  this test shows that you can be your own friend
   def test_NoRestrictionsBeingMyOwnFriend
       bfound = 0;
-      user = User.new(:username=>'forest', :password_hash=>Digest::SHA1.hexdigest("gump"), :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
+      user = User.new(:username=>'forest', :password=>"gump", :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
       user.friends.concat user
       @friends_all = User.find :all, :order => 'last_name ASC, first_name ASC'  
       user.friends.each do |eUser |
@@ -43,8 +43,8 @@ class UserFriendsTest < Test::Unit::TestCase
 #  this test shows that you can all user's excluding this user
   def test_addFriends
       bFound = false;
-      user1 = User.new(:username=>'forest1', :password_hash=>Digest::SHA1.hexdigest("gump1"), :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
-      user2 = User.new(:username=>'forest2', :password_hash=>Digest::SHA1.hexdigest("gump2"), :start_year=>'2007', :start_sem=>'FA', :birthday=>'1930-10-29', :college => College.find(:first))
+      user1 = User.new(:username=>'forest1', :password=>"gump1", :start_year=>'2009', :start_sem=>'FA', :birthday=>'1990-10-29', :college => College.find(:first))
+      user2 = User.new(:username=>'forest2', :password=>"gump2", :start_year=>'2007', :start_sem=>'FA', :birthday=>'1930-10-29', :college => College.find(:first))
       user1.save
       user2.save
       @friends_all = User.find :all, :order => 'last_name ASC, first_name ASC'  
