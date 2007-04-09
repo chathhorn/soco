96c84e8c8107cd7d9ef17d7b1b8c960211819719
nsonie2 <nsonie2@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-09 02:20:31 +0000

    added feature so that list the courses thet user will plan to take with his friend.
    bug: this created bug in friends controller.
         Cant seem to add new friends. 
    issue 009

--- app/models/register_with_friend.rb
+++ app/models/register_with_friend.rb
@@ -1,4 +1,2 @@
 class RegisterWithFriend < ActiveRecord::Base
-    has_many :friends_users
-    has_many :cis_courses
 end
---
+++ test/unit/friends_users_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class FriendsUsersTest < Test::Unit::TestCase
+  fixtures :friends_users
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
