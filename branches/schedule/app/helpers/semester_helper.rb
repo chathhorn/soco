516f11fc2c1fa8c10e6b7d89de3b0da353985f1d
hathhorn <hathhorn@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-11 01:50:20 +0000

    issue 13

--- branches/schedule/app/helpers/semester_helper.rb
+++
@@ -1,8 +0,0 @@
-module SemesterHelper
-  def classes day
-    @semester = Semester.find(params[:id])
-    render :partial => 'section', 
-      :collection => @semester.course_plan.cis_sections.find(:all, 
-      :conditions =>["days LIKE ?",'%'+day+'%'])
-  end
-end
