d2f39ad9a7ae382af1ae979beaaf12611b46c60c
szalko <szalko@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-12 02:19:16 +0000

    Test Cleanup Replaced all instances in tests and fixtures that incorrectly created users with passwords < 6 chars issue 129

--- test/unit/semester_test.rb
+++ test/unit/semester_test.rb
@@ -2,12 +2,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class SemesterTest < Test::Unit::TestCase
   fixtures :semesters
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-  
   
  def test_create_semesters
     #Create a new user with the specified attributes
