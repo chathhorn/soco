12e28147fe9b3eede9d044ba1a8dacd8e2b69c39
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-25 06:22:25 +0000

    Added new course data file and modified perl script to import it issue 165

--- trunk/components/local_database_creation/create_database.pl
+++ trunk/components/local_database_creation/create_database.pl
@@ -1,8 +1,17 @@
 #!env perl -w
 
+use Compress::Zlib;
+use Getopt::Long;
+
+$create = '';
+
+GetOptions(
+	'create' => \$create
+	);
+
 $ENV{PATH} = $ENV{PATH} . ';c:\Program Files\mySQL\MySQL Server 5.0\bin';
 
-if ($#ARGV >= 0 && $ARGV[0] eq '--create') {
+if ($create) {
 	print "Enter MySQL password for user 'root': ";
 	$password = <STDIN>;
 
@@ -13,16 +22,28 @@ if ($#ARGV >= 0 && $ARGV[0] eq '--create') {
 chdir("../..");
 
 print "[**   ] Running migrations to create tables\n";
-system("rake db:migrate") and die "Migration failed\n";
+	system("rake db:migrate") and die "Migration failed\n";
 
 print "[***  ] Clearing existing CIS course data\n";
-system("mysql -usoco -pmonkey soco_development < \"db/data/development/TRUNCATE CIS Data.sql\"") and die "Truncate failed\n";
+	system("mysql -usoco -pmonkey soco_development < \"db/data/development/TRUNCATE CIS Data.sql\"") and die "Truncate failed\n";
 
 print "[**** ] Importing CIS course data\n";
-system("mysql -usoco -pmonkey soco_development < \"db/data/development/CIS Data.sql\"") and die "Data import failed\n";
+
+	my $gz = gzopen("db/data/development/CIS Data.sql.gz", "rb") or die "Data import failed\n";
+
+	open(MYSQL, "| mysql -usoco -pmonkey soco_development") or die "Data import failed\n";
+
+	print MYSQL $buffer while ($gz->gzread($buffer) > 0);
+
+	die "Error reading from db/data/development/CIS Data.sql.gz: $gzerrno " . ($gzerrno+0) . "\n" 
+		if $gzerrno != Z_STREAM_END ;
+
+	$gz->gzclose();
+
+	close(MYSQL);
 
 print "[*****] Creating testing tables\n";
-system("rake db:test:clone_structure") and die "Testing table creation failed\n";
+	system("rake db:test:clone_structure") and die "Testing table creation failed\n";
 
 print "Finished!\n";
 
