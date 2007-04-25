4b405d9d94c4abbbfc05bdba81777488cf8783db
hathhorn <hathhorn@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-25 03:56:47 +0000

    split semester view from long term view
    issue 13

--- app/helpers/long_term_helper.rb
+++ app/helpers/long_term_helper.rb
@@ -29,24 +29,4 @@ module LongTermHelper
     end
   end
 
-  # brought over for semester view
-  def classes day, semester
-    semester == -1 && return
-    @semester = Semester.find(semester)
-    render :partial => 'section', 
-      :collection => @semester.course_plan.cis_sections.find(:all, 
-      :conditions =>["days LIKE ?",'%'+day+'%'])
-  end
-
-  def section_in_plan?(section, semester)
-        begin
-              Semester.find(semester).course_plan.cis_sections.find section
-        rescue
-              return false
-        else
-              return true
-        end
-  end
-
-
 end
