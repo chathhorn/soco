8c9a08203701f6bf715ef102aa691e78fc2e49eb
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-28 21:29:29 +0000

    modified so course schedule links show proper semester issue 190

--- trunk/app/models/cis_semester.rb
+++ trunk/app/models/cis_semester.rb
@@ -1,4 +1,15 @@
 class CisSemester < ActiveRecord::Base
   has_many :cis_sections
   belongs_to :cis_course
+
+  def semester_word
+    case semester
+      when :SP
+        "Spring"
+      when :FA
+        "Fall"
+      when :SU
+        "Summer"
+    end
+  end
 end
