d7c5c584261f4339b1d1b748e2a9a08f0819a88e
chorneck <chorneck@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-30 09:08:03 +0000

    Fixed an error in the test_see_my_schedule function.  Not pretty, but it works issue 203

--- trunk/test/unit/friend_mailer_test.rb
+++ trunk/test/unit/friend_mailer_test.rb
@@ -17,12 +17,24 @@ class FriendMailerTest < Test::Unit::TestCase
   end
 
   def test_see_my_schedule
-    @expected.subject = 'FriendMailer#see_my_schedule'
-    @expected.body    = read_fixture('see_my_schedule')
-    @expected.date    = Time.now
+    #@expected.subject = 'FriendMailer#see_my_schedule'
+    #@expected.body    = read_fixture('see_my_schedule')
+    #@expected.date    = Time.now
+
+    #Hackish
+    body =  " Kathleen Reilly, would you like to join Chris Horneck in any of these classes? CS 428\n\n "\
+            << "Use your email address (reillyke@gmail.com) to log in if you are not "\
+            << "already registered with soco. http://localhost:3000/login"
+            
+    subject =  "Take a look at Chris Horneck's classes for the following semester"       
+           
+    response = FriendMailer.create_see_my_schedule("Chris Horneck", "chorneck@gmail.com", "Kathleen Reilly", "reillyke@gmail.com", "CS 428")
+    assert_equal body, response.body
+    assert_equal subject, response.subject
+    assert_equal "reillyke@gmail.com", response.to[0]
     
     #(my_name, my_email, friend_name, friend_email, classes_shared)
-    assert_equal @expected.encoded, FriendMailer.create_see_my_schedule(@expected.date).encoded
+    #assert_equal @expected.encoded, FriendMailer.create_see_my_schedule(@expected.date).encoded
   end
 
   private
