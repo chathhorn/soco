84fb7ca65440bc58ea6025154c820c036385dbc3
chorneck <chorneck@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-30 08:34:08 +0000

    More unit testing issue 196

--- trunk/test/unit/cis_semester_test.rb
+++ trunk/test/unit/cis_semester_test.rb
@@ -3,8 +3,16 @@ require File.dirname(__FILE__) + '/../test_helper'
 class CisSemesterTest < Test::Unit::TestCase
   fixtures :cis_semesters
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_semester_word
+  
+  spring = cis_semesters(:first)
+  fall = cis_semesters(:another)  
+  summer = cis_semesters(:third)  
+  
+  assert_equal("Spring", spring.semester_word)
+  assert_equal("Fall", fall.semester_word)
+  assert_equal("Summer", summer.semester_word)
+  
   end
+
 end
