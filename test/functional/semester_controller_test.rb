ea9c0624eff1fec1d6191f36d2de291059dd74d7
chorneck <chorneck@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-04-30 19:56:11 +0000

    Wrote a few tests to exercise some of the functions in semester_controller issue 211

--- test/functional/semester_controller_test.rb
+++ test/functional/semester_controller_test.rb
@@ -5,14 +5,44 @@ require 'semester_controller'
 class SemesterController; def rescue_action(e) raise e end; end
 
 class SemesterControllerTest < Test::Unit::TestCase
+  fixtures :cis_sections, :cis_courses_semesters, :cis_courses, :semesters, :users
+
   def setup
     @controller = SemesterController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    @request.session[:user] = 1
+
   end
 
   # Replace this with your real tests.
   def test_truth
     assert true
   end
+  
+  def test_generate_next
+    get :generate_next, {'id' => "1"}
+    assert_response :success
+  end
+  
+  def test_generate_prev
+    get :generate_prev, {'id' => "1"}
+    assert_response :success
+  end
+  
+  def test_toggle_section
+    get :toggle_section, {'id'=> "1", 'section'=>"1"}
+    assert_response :success
+  end
+  
+  def test_show
+    get :show, {'id'=> "1"}
+    assert_response :success
+  end
+  
+  def test_show_previews
+    get :show_previews, {'id'=> "1"}
+    assert_response :success
+  end
+  
 end
