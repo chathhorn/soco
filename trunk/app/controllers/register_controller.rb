695ba8f519086347d79ff835cd537673d81d9dd7
mcowell <mcowell@d63a77b0-c42f-0410-a0cb-d9e7f84b85d9>
2007-05-01 00:43:56 +0000

    updated developer documentation (1/2) issue 143

--- trunk/app/controllers/register_controller.rb
+++ trunk/app/controllers/register_controller.rb
@@ -1,5 +1,6 @@
 class RegisterController < ApplicationController
 
+  #lists the CRNS for a particular semseter
   def list_crns
     user = User.find session[:user]
     plan = user.semesters.find(params[:id]).course_plan
